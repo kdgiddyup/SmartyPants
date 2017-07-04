cce8d900491e74574c80fde2cacdc40f711022a8
Kelly Davis <kellydavis1974@gmail.com>
2017-07-04 04:10:18 -0400

    trying out basic-auth-mongoose

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,16 +1,22 @@
 var path = require("path");
 var axios= require("axios");
 
+// this will change depending on environment
+var resourceHost = process.env.DEV_API || "https://smartlyricsapi.herokuapp.com";
+
+console.log("Using this api resource:",resourceHost);
+
 module.exports = function(app) {
+
 /********************
-    Mongoose handlers
- ********************/
+  Mongoose handlers
+********************/
 
   // Route to save our favorited song to mongoDB via mongoose
   app.post("/api/favorites", function(req, res) {
     // req.body should include title , artist, song_id, image url, lyrics page url
     // send it to our smartlyrics api:
-    axios.post("https://smartlyricsapi.herokuapp.com/api/favorites",req.body)
+    axios.post(`${resourceHost}/api/favorites`,req.body)
         .then(function(response){
             res.json(response.data)
         })
@@ -22,7 +28,8 @@ module.exports = function(app) {
 // Route to retrieve and show favorited articles
 app.get("/api/favorites", function(req,res){
   // find favorites of currently logged-in user
-  axios.get("https://smartlyricsapi.herokuapp.com/api/favorites")
+ console.log("FAVORITES HIT");
+ axios.get(`${resourceHost}/api/favorites`)
     .then(function(response){
         res.json(response.data)
     })
@@ -33,7 +40,7 @@ app.get("/api/favorites", function(req,res){
 
 // Route to remove favorited article
 app.get("/api/remove/:id", function(req,res){
-  axios.get(`https://smartlyricsapi.herokuapp.com/api/remove/${req.params.id}`)
+  axios.get(`${resourceHost}/api/remove/${req.params.id}`)
   .then(function(response){
       console.log("remove song raw response:",response.data);
       res.send(response.data)
@@ -47,8 +54,8 @@ app.get("/api/remove/:id", function(req,res){
 Genius api handlers
 ******************/
 // send search terms to my api, which are then sent off to Genius api
-app.post("/api/search/", function(req, res) {
-    axios.post("https://smartlyricsapi.herokuapp.com/api/search",req.body)
+app.post("/api/search", function(req, res) {
+    axios.post(`${resourceHost}/api/search`,req.body)
         .then(function(response){
             res.json(response.data)
         })
@@ -59,7 +66,7 @@ app.post("/api/search/", function(req, res) {
 
  // search for annotations on lyrics panels
 app.get("/api/annotation/:id", function(req, res) {
-    axios.get(`https://smartlyricsapi.herokuapp.com/api/annotation/${req.params.id}`)
+    axios.get(`${resourceHost}/api/annotation/${req.params.id}`)
         .then(function(response){
             res.json(response.data)
         })
@@ -70,7 +77,7 @@ app.get("/api/annotation/:id", function(req, res) {
 
 // scrape lyrics -- this is for DEMO only. Genius API prohibits scraping for lyrics in production apps
 app.post("/api/lyrics/", function(req, res) {
-    axios.post("https://smartlyricsapi.herokuapp.com/api/lyrics",req.body)
+    axios.post(`${resourceHost}/api/lyrics`,req.body)
         .then(function(response){
             res.json(response.data)
         })
@@ -80,13 +87,8 @@ app.post("/api/lyrics/", function(req, res) {
     });
 
 
-
-
-
  /* GET Home Page */
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "/../public/home.html"));
 });
 }; // end module export
-
-
